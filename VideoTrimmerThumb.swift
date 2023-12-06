e786fbc0fddbf2e4e00949c49c91c814065c19dc
Anthony Hoang <anthony.hoang720@gmail.com>
2023-12-06 15:33:54 -0700

    expose colors

--- VideoTrimmerThumb.swift
+++ VideoTrimmerThumb.swift
@@ -9,111 +9,110 @@
 import UIKit
 
 class VideoTrimmerThumb: UIView {
-	var isActive = false
+    var isActive = false
 
-	var leadingChevronImageView = UIImageView(image: UIImage(systemName: "chevron.compact.left"))
-	var trailingChevronView = UIImageView(image: UIImage(systemName: "chevron.compact.right"))
+    var leadingChevronImageView = UIImageView(image: UIImage(systemName: "chevron.compact.left"))
+    var trailingChevronView = UIImageView(image: UIImage(systemName: "chevron.compact.right"))
 
-	var wrapperView = UIView()
-	var leadingView = UIView()
-	var trailingView = UIView()
-	var topView = UIView()
-	var bottomView = UIView()
+    var wrapperView = UIView()
+    var leadingView = UIView()
+    var trailingView = UIView()
+    var topView = UIView()
+    var bottomView = UIView()
 
-	let leadingGrabber = UIControl()
-	let trailingGrabber = UIControl()
+    let leadingGrabber = UIControl()
+    let trailingGrabber = UIControl()
 
-	let chevronWidth = CGFloat(16)
-	let edgeHeight = CGFloat(4)
+    let chevronWidth = CGFloat(16)
+    let edgeHeight = CGFloat(4)
 
-	// MARK: - Input
-	@objc private func x(_ sender: Any) {
+    // MARK: - Input
+    @objc private func x(_ sender: Any) {
 
-	}
+    }
 
-	// MARK: - Private
-	private func updateColor() {
-		let color = UIColor.systemYellow
-		leadingView.backgroundColor = color
-		trailingView.backgroundColor = color
-		topView.backgroundColor = color
-		bottomView.backgroundColor = color
-	}
+    // MARK: - Private
+    public func updateColor(_ color: UIColor) {
+        leadingView.backgroundColor = color
+        trailingView.backgroundColor = color
+        topView.backgroundColor = color
+        bottomView.backgroundColor = color
+    }
 
-	private func setup() {
+    private func setup() {
 
-		leadingChevronImageView.contentMode = .scaleAspectFill
-		trailingChevronView.contentMode = .scaleAspectFill
+        leadingChevronImageView.contentMode = .scaleAspectFill
+        trailingChevronView.contentMode = .scaleAspectFill
 
-		leadingChevronImageView.tintColor = .white
-		trailingChevronView.tintColor = .white
+        leadingChevronImageView.tintColor = .white
+        trailingChevronView.tintColor = .white
 
-		leadingChevronImageView.tintAdjustmentMode = .normal
-		trailingChevronView.tintAdjustmentMode = .normal
+        leadingChevronImageView.tintAdjustmentMode = .normal
+        trailingChevronView.tintAdjustmentMode = .normal
 
-		leadingView.layer.cornerRadius = 6
-		leadingView.layer.cornerCurve = .continuous
-		leadingView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+        leadingView.layer.cornerRadius = 6
+        leadingView.layer.cornerCurve = .continuous
+        leadingView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
 
-		trailingView.layer.cornerRadius = 6
-		trailingView.layer.cornerCurve = .continuous
-		trailingView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+        trailingView.layer.cornerRadius = 6
+        trailingView.layer.cornerCurve = .continuous
+        trailingView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
 
-		leadingView.addSubview(leadingChevronImageView)
-		trailingView.addSubview(trailingChevronView)
+        leadingView.addSubview(leadingChevronImageView)
+        trailingView.addSubview(trailingChevronView)
 
-		//		wrapperView.layer.shadowColor = UIColor.black.cgColor
-		//		wrapperView.layer.shadowOffset = .zero
-		//		wrapperView.layer.shadowRadius = 2
-		//		wrapperView.layer.shadowOpacity = 0.25
+        //        wrapperView.layer.shadowColor = UIColor.black.cgColor
+        //        wrapperView.layer.shadowOffset = .zero
+        //        wrapperView.layer.shadowRadius = 2
+        //        wrapperView.layer.shadowOpacity = 0.25
 
-		wrapperView.addSubview(leadingView)
-		wrapperView.addSubview(trailingView)
-		wrapperView.addSubview(topView)
-		wrapperView.addSubview(bottomView)
-		addSubview(wrapperView)
+        wrapperView.addSubview(leadingView)
+        wrapperView.addSubview(trailingView)
+        wrapperView.addSubview(topView)
+        wrapperView.addSubview(bottomView)
+        addSubview(wrapperView)
 
-		wrapperView.addSubview(leadingGrabber)
-		wrapperView.addSubview(trailingGrabber)
+        wrapperView.addSubview(leadingGrabber)
+        wrapperView.addSubview(trailingGrabber)
 
-		updateColor()
-	}
-
+        updateColor(.systemYellow)
+    }
 
 
-	// MARK: - UIView
 
-	override func layoutSubviews() {
-		super.layoutSubviews()
+    // MARK: - UIView
 
-		let size = bounds.size
+    override func layoutSubviews() {
+        super.layoutSubviews()
 
-		wrapperView.frame = CGRect(origin: .zero, size: size)
+        let size = bounds.size
 
-		leadingView.frame = CGRect(x: 0, y: 0, width: chevronWidth, height: bounds.height)
-		trailingView.frame = CGRect(x: bounds.width - chevronWidth, y: 0, width: chevronWidth, height: bounds.height)
-		topView.frame = CGRect(x: chevronWidth, y: 0, width: bounds.width - chevronWidth * 2, height: edgeHeight)
-		bottomView.frame = CGRect(x: chevronWidth, y: bounds.height - edgeHeight, width: bounds.width - chevronWidth * 2, height: edgeHeight)
+        wrapperView.frame = CGRect(origin: .zero, size: size)
 
-		let chevronHorizontalInset = CGFloat(2)
-		let chevronVerticalInset = CGFloat(8)
-		let chevronFrame = CGRect(x: chevronHorizontalInset, y: chevronVerticalInset, width: chevronWidth - chevronHorizontalInset * 2, height: size.height - chevronVerticalInset * 2)
+        leadingView.frame = CGRect(x: 0, y: 0, width: chevronWidth, height: bounds.height)
+        trailingView.frame = CGRect(x: bounds.width - chevronWidth, y: 0, width: chevronWidth, height: bounds.height)
+        topView.frame = CGRect(x: chevronWidth, y: 0, width: bounds.width - chevronWidth * 2, height: edgeHeight)
+        bottomView.frame = CGRect(x: chevronWidth, y: bounds.height - edgeHeight, width: bounds.width - chevronWidth * 2, height: edgeHeight)
 
-		leadingChevronImageView.frame = chevronFrame
-		trailingChevronView.frame = chevronFrame
+        let chevronHorizontalInset = CGFloat(2)
+        let chevronVerticalInset = CGFloat(8)
+        let chevronFrame = CGRect(x: chevronHorizontalInset, y: chevronVerticalInset, width: chevronWidth - chevronHorizontalInset * 2, height: size.height - chevronVerticalInset * 2)
 
-		leadingGrabber.frame = leadingView.frame
-		trailingGrabber.frame = trailingView.frame
-	}
+        leadingChevronImageView.frame = chevronFrame
+        trailingChevronView.frame = chevronFrame
 
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		setup()
-	}
+        leadingGrabber.frame = leadingView.frame
+        trailingGrabber.frame = trailingView.frame
+    }
 
-	required init?(coder: NSCoder) {
-		super.init(coder: coder)
-		setup()
-	}
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
 }
 
